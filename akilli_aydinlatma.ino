40c908b6736bdb1326d93eef9b8f458f026722bd
Çivi <96166551+batuhanbartu@users.noreply.github.com>
2024-01-15 11:20:08 +0300

    Yorum satırı eklenmiştir akilli_aydinlatma.ino

--- akilli_aydinlatma.ino
+++ akilli_aydinlatma.ino
@@ -1,25 +1,20 @@
-int led=3;
+int led = 3; // LED için kullanılacak pin numarasını tanımlar
 
-void setup()
-{
-Serial.begin(9600);
-pinMode (led, OUTPUT);
+void setup() {
+  Serial.begin(9600); // Seri iletişim başlatılır, baud hızı 9600 olarak ayarlanır
+  pinMode(led, OUTPUT); // LED pinini çıkış modunda ayarlar
 }
 
-void loop()
-{
-  int ldr_sensor_deger=analogRead(A0);
-  Serial.println(ldr sensor_deger);
+void loop() {
+  int ldr_sensor_deger = analogRead(A0); // A0 pininden LDR sensörünün analog değerini okur
+  Serial.println(ldr_sensor_deger); // LDR sensör değerini seri port üzerinden bilgisayara gönderir
 
-if (ldr_sensor_deger<=250)
-{
-  digitalWrite(led, HIGH);
-}
+  if (ldr_sensor_deger <= 250) { 
+    digitalWrite(led, HIGH); // Eğer LDR sensör değeri 250'den küçük veya eşitse, LED'i yak
+  } 
+  else {
+    digitalWrite(led, LOW); // Değilse, LED'i söndür
+  }
 
-else
-{
-  digitalWrite(led, LOW);
-}
-
-delay(500);
+  delay(500); // 500 milisaniye bekleme süresi ekler (yarım saniye)
 }
